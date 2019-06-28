8efc3c90a2b13b07ef24b8fda1f23bd5444738ae
Ihar <yb.igrok.by@gmail.com>
2019-06-28 12:00:33 +0300

    refactor: delete irrelevant checks

--- sources/models/activitiesdata.js
+++ sources/models/activitiesdata.js
@@ -5,12 +5,8 @@ const strFormatTime = webix.Date.dateToStr("%H:%i");
 export const activities = new webix.DataCollection({
 	scheme:{
 		$init: (obj) => {
-			if (!obj.ConvDueDate) {
-				obj.ConvDueDate = dateFormat(obj.DueDate);
-			}
-			if (!obj.ConvDueTime) {
-				obj.ConvDueTime = obj.ConvDueDate;
-			}
+			obj.ConvDueDate = dateFormat(obj.DueDate);
+			obj.ConvDueTime = obj.ConvDueDate;
 		},
 		$save: (obj) => {
 			obj.DueDate =`${strFormatDate(obj.ConvDueDate)} ${strFormatTime(obj.ConvDueTime)}`;
