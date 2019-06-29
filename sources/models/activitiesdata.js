129bc1fec7321e3ebfc9aa65f720fb136f2cb150
Ihar <yb.igrok.by@gmail.com>
2019-06-29 13:31:04 +0300

    add missed merged files

--- sources/models/activitiesdata.js
+++ sources/models/activitiesdata.js
@@ -5,13 +5,8 @@ const strFormatTime = webix.Date.dateToStr("%H:%i");
 export const activities = new webix.DataCollection({
 	scheme:{
 		$init: (obj) => {
-<<<<<<< HEAD
-			obj.ConvDueDate = new Date(dateFormat(obj.DueDate));
-			obj.ConvDueTime = new Date(obj.ConvDueDate);
-=======
 			obj.ConvDueDate = dateFormat(obj.DueDate);
 			obj.ConvDueTime = obj.ConvDueDate;
->>>>>>> origin/master
 		},
 		$save: (obj) => {
 			obj.DueDate =`${strFormatDate(obj.ConvDueDate)} ${strFormatTime(obj.ConvDueTime)}`;
--- sources/views/contacts/contactslist.js
+++ sources/views/contacts/contactslist.js
@@ -33,21 +33,12 @@ export default class ContactsView extends JetView{
 			css: "persons_list",
 			type:{
 				template:obj => `
-<<<<<<< HEAD
-							<image class="contactphoto" src="${obj.Photo ? obj.Photo : "https://upload.wikimedia.org/wikipedia/commons/2/2f/No-photo-m.png"}" />
-							<div class="text">
-								<span class="contactname">${obj.FirstName} ${obj.LastName}</span>
-								<span class="contactjob">${obj.Job}</span>
-							</div>
-							`,
-=======
                 <image class="contactphoto" src="${obj.Photo || "https://upload.wikimedia.org/wikipedia/commons/2/2f/No-photo-m.png"}" />
                 <div class="text">
                     <span class="contactname">${obj.FirstName || "-"} ${obj.LastName || "-"}</span>
                     <span class="contactjob">${obj.Job || "-"}</span>
                 </div>
                 `,
->>>>>>> origin/master
 				height:66
 			},
 			on: {
