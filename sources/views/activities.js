dd8549c065935ec6db035bc26578894a4d6808f6
Ihar <yb.igrok.by@gmail.com>
2019-07-03 14:12:35 +0300

    feat: create basic setting view

--- sources/views/activities.js
+++ sources/views/activities.js
@@ -5,12 +5,47 @@ import ActivityWindow from "./activities/activityform";
 
 export default class DataView extends JetView{
 	config(){
+		const tabbar = {
+			view: "tabbar",
+			id: "selector",
+			gravity: 3,
+			borderless: true,
+			options: [
+				{
+					id: 1, value: "All"
+				},
+				{
+					id: 2, value: "Overdue"
+				},
+				{
+					id: 3, value: "Completed"
+				},
+				{
+					id: 4, value: "Today"
+				},
+				{
+					id: 5, value: "Tommorow"
+				},
+				{
+					id: 6, value: "This week"
+				},
+				{
+					id: 7, value: "This month"
+				}
+			],
+			on: {
+				onChange: () => {
+					webix.$$("activities:datatable").filterByAll();
+				}
+			}
+		};
+
 		return {
 			rows: [
 				{	
 					view:"toolbar", css:"subbar", padding:0,
 					elements:[
-						{},
+						tabbar,
 						{
 							view: "button",
 							label: "Add",
@@ -33,11 +68,50 @@ export default class DataView extends JetView{
 		};
 	}
 
-	ready() {
+	ready(view) {
 		this.table = webix.$$("activities:datatable");
 		this.table.attachEvent("onAfterSelect", (id) => { 
 			this.show(`../activities?id=${id}`);
 		});
+		
+		view.queryView("datatable").registerFilter(
+			this.$$("selector"),
+			{
+				columnId: ["State"],
+				compare: (value, filter) => {
+					if (filter == 3) {
+						return value == 1;
+					} else {
+						return value;
+					}
+				}
+			},
+			// {
+			// 	columnId: "ConvDueDate",
+			// 	compare: (value, filter, item)	=> {
+			// 		// const currentDate = new Date();
+			// 		// let date = new Date(value);
+			// 		if (filter == 2) {
+			// 			// console.log(value);
+			// 			return value.getFullYear() < 2010;
+			// 		// } else if (filter == 3) {
+			// 		// 	return value > 1980 && value <= 2000;
+			// 		// } else if (filter == 4) {
+			// 		// 	return value > 2000 && value <= currentYear.getFullYear();
+			// 		} else {
+			// 			return value;
+			// 		}
+			// 	}
+			// },
+			{
+				getValue: function(node) {
+					return node.getValue();
+				},
+				setValue: function(node, value) {
+					node.setValue(value);
+				}
+			}
+		);
 	}
 
 	init() {
--- sources/views/settings.js
+++ sources/views/settings.js
@@ -1,3 +1,179 @@
-export default {
-	template:"Gonna make something incredible", css:"webix_shadow_medium app_start"
-};
+import {JetView} from "webix-jet";
+import { activitytypes } from "../models/activitytypesdata";
+import { statuses } from "../models/statusesdata";
+
+export default class SettingsView extends JetView {
+	config() {
+		// const lang = this.app.getService("locale").getLang();
+		// const _ = this.app.getService("locale")._;
+
+		const selector = {
+			view: "segmented",
+			inputWidth: 250,
+			id: "lng",
+			options: [
+				{id: "en", value: "EN"},
+				{id: "ru", value: "RU"}
+			],
+			// click: () => this.toggleLanguage(),
+			// value: lang
+		};
+
+		const activityTypesTable = {
+			view: "datatable",
+			localId: "activityTypesTable",
+			editable: true,
+			scroll: "auto",
+			editaction: "dblclick",
+			columns: [
+				{
+					id:"Value",
+					header:"Activity Type",
+					fillspace: true,
+					editor: "text"
+				},
+				{
+					id:"Icon",
+					header:"Icon",
+					width: 150,
+					editor: "text"
+				},
+				{
+					id: "",
+					template: "{common.trashIcon()}",
+					width: 60
+				}
+			],
+			onClick: {
+				"wxi-trash":(e, id) => {
+					webix.confirm({
+						text: "Are you sure you want to delete it?"
+					}).then( () => {
+						activitytypes.remove(id);
+					});
+					return false;
+				}
+			}
+		};
+
+		const statusesTable = {
+			view: "datatable",
+			localId: "statusesTable",
+			editable: true,
+			scroll: "auto",
+			editaction: "dblclick",
+			columns: [
+				{
+					id:"Value",
+					header:"Status",
+					fillspace: true,
+					editor: "text"
+				},
+				{
+					id:"Icon",
+					header:"Icon",
+					width: 150,
+					editor: "text"
+				},
+				{
+					id: "",
+					template: "{common.trashIcon()}",
+					width: 60
+				}
+			],
+			onClick: {
+				"wxi-trash":(e, id) => {
+					webix.confirm({
+						text: "Are you sure you want to delete it?"
+					}).then( () => {
+						statuses.remove(id);
+					});
+					return false;
+				}
+			}
+		};
+
+		const activityTypesLabel = {
+			view: "label",
+			label: "Activity Types",
+			align: "center",
+			localId: "label",
+			css: "settings_label"
+		};
+
+		const statusesLabel = {
+			view: "label",
+			label: "Statuses",
+			align: "center",
+			localId: "label",
+			css: "settings_label"
+		};
+
+		const buttons = {	
+			view:"toolbar", css:"subbar", padding:0,
+			elements:[
+				{},
+				{
+					view: "button",
+					label: "Add type",
+					localId: "addButton",
+					type:"icon",
+					icon: "wxi-plus",
+					css: "webix_primary",
+					click: () => {
+						activitytypes.add({Value: "value", Icon: "icon"});
+					}
+				},
+				{
+					gravity: 2
+				},
+				{
+					view: "button",
+					label: "Add status",
+					localId: "addButton",
+					type:"icon",
+					icon: "wxi-plus",
+					css: "webix_primary",
+					click: () => {
+						statuses.add({Value: "name", Icon: "icon"});
+					}
+				},
+				{}
+			]
+		};
+
+		return {
+			rows: [
+				selector,
+				{
+					cols: [
+						{
+							rows: [
+								activityTypesLabel,
+								activityTypesTable
+							]
+						},
+						{
+							rows: [
+								statusesLabel,
+								statusesTable
+							]
+						}
+					]
+				},
+				buttons
+			]
+		};
+	}
+
+	init() {
+		this.$$("activityTypesTable").sync(activitytypes);
+		this.$$("statusesTable").sync(statuses);
+	}
+
+	// toggleLanguage() {
+	// 	const langs = this.app.getService("locale");
+	// 	const value = this.$$("lng").getValue();
+	// 	langs.setLang(value);
+	// }
+}
